6589386c8716cd0db9a84364563ffe8ece7c1fd1
Fede <federicous@gmail.com>
2021-12-29 00:49:20 -0300

    actualizo tabla

--- index.js
+++ index.js
@@ -21,25 +21,38 @@ app.get("/", async(req, res, next)=>{
 
 let misProductos = new Contenedor('./public/productos.txt')
 let productos= [];
+let dataArray=[];
 
 let httpServer = new HttpServer(app);
 let socketIOServer = new SocketIO(httpServer);
 
-socketIOServer.on('connection', socket =>{
-	socket.on('fillP', data =>{
-		let res = {
+socketIOServer.on('connection', async socket =>{
+	let misProductosGuardados= await misProductos.getAll()
+	await socket.on('fillP', async data =>{
+		/* let res = {
 			id: socket.id,
 			data
-		}
-		productos.push(res)
+		} */
+		await misProductos.save(data)
+		dataArray.push(data)
+		console.log(data);
+		// console.log(dataArray);
+		// productos.push(res)
 		// replico la info a todos los conectados
-		socketIOServer.sockets.emit('listenserver', productos)
+		// socketIOServer.sockets.emit('listenserver', productos)
+		// socketIOServer.sockets.emit('listenserver', dataArray)
+		await socketIOServer.sockets.emit('listenserver', misProductosGuardados)
 	})
+	// await socketIOServer.sockets.emit('listenserver', misProductosGuardados)
+
 	socket.emit('MI SALA', 'hola, desde la sala');
+	await socket.emit('listenserver', misProductosGuardados)
 	console.log(`Nuevo usuario: ${socket.id}`);
+	console.log(`data arrray: ${dataArray}`);
 	// Utilizo el archivo
 	
 })
+// socketIOServer.init();
 
 httpServer.listen(PORT, ()=>{
 	console.log(`Server on!: http://localhost:${PORT}`)
